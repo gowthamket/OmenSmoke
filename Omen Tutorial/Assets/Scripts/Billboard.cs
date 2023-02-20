6218e0404df605ac6bc05f4e063fe094197092f5
gowthamket <65178336+gowthamket@users.noreply.github.com>
2023-02-20 11:37:38 -0500

    Initial Commit

---
+++ Omen Tutorial/Assets/Scripts/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+
+    [SerializeField] private Var_Camera m_camera;
+    private void Update()
+    {
+        if (m_camera.Value != null)
+            transform.rotation = Quaternion.LookRotation(m_camera.Value.transform.forward);
+    }
+
+}
